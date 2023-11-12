641bb29fe798257ce06d8c9577ebb47a89380d21
zhaoyi <banbaoyan_zy@163.com>
2023-11-12 21:15:16 +0800

    react

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -16,23 +16,11 @@ module.exports = {
     },
     module: {
         rules: [
-            // {
-            //     test: /\.ts|.tsx$/,
-            //     exclude: /node_modules/,
-            //     use: [
-            //         {
-            //             loader: 'babel-loader',
-            //             // options: {
-            //             //     presets: ['@babel/preset-typescript']
-            //             // }
-            //         }
-            //     ],
-            // },
             {
-                test: /\.js|.ts|.tsx$/,
+                test:  /\.(jsx?|tsx?)$/,
                 exclude: /node_modules/,
                 use: { 
-                    loader: 'babel-loader' 
+                    loader: 'babel-loader',
                 }
             },
             {
@@ -42,15 +30,7 @@ module.exports = {
                     dataUrlCondition: {
                         maxSize: 10 * 1024, // 小于10kb的图片会被base64处理
                     },
-                },
-                // generator: {
-                //     // 将图片文件输出到 static/imags 目录中
-                //     // 将图片文件命名 [hash:8][ext][query]
-                //     // [hash:8]: hash值取8位
-                //     // [ext]: 使用之前的文件扩展名
-                //     // [query]: 添加之前的query参数
-                //     filename: "imags/[hash:8][ext][query]",
-                // },
+                }
             },
             {
                 test: /\.(ttf|woff2?|map4|map3|avi)$/,
